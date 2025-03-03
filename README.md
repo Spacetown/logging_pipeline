7f80e8c8393771baba1ad5a311d1c11a1dc5459f
Spacetown <40258682+spacetown@users.noreply.github.com>
2025-03-03 19:50:01 +0100

    First implementation

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # logging_pipeline
-Python module to inject logging handler for printing message in the format captured by pipeline
+
+Python module to inject logging handler for printing message in the format captured by pipeline.
+
+Install with:
+
+```bash
+pip install logging_pipeline
+```
+
+If logging is imported a handler is added to the root logger to print the error and warning messages
+in addition with a format recognized by the pipeline environment executing the script. If the script
+is executed outside a pipeline nothing is printed. Supported pipelines are:
+
+- Azure pipelines
+- GitHub actions
+
+## Compatibility
+
+`logging_pipeline` uses a live-patching system to target the `logging` library and add a handler.
+
+## PyInstaller
+
+The method used to automatically adding a logging handler relies on a .pth file script that python
+loads at startup. This method does not work when a python application is bundled into an executable
+with PyInstaller (or similar).
+If you want to use this tool in an application built with PyInstaller it will need to be manually
+enabled in your application.
+This can be done by adding the following line to the top of your main application script:
+
+```python
+import logging_pipeline.wrapt_logging
+```
+
+This must be run before logging is imported.
+
+## Acknowledgements
+
+The method of patching at runtime is built from the [pip_system_certs](https://pypi.org/project/pip-system-certs/)
+module.
---
+++ tests/run_tests.sh
@@ -0,0 +1,98 @@
+#!/usr/bin/env bash
+set -e
+
+THIS_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+
+echo """
+Running ruff format..."""
+ruff format --check $THIS_DIRECTORY/..
+
+echo """
+Running ruff check..."""
+ruff check $THIS_DIRECTORY/..
+
+echo """
+Running bandit..."""
+bandit -c pyproject.toml -r $THIS_DIRECTORY/..
+
+
+echo """
+Creating virtual environment and install wheel..."""
+python3 -m venv --clear /tmp/.venv
+. /tmp/.venv/bin/activate
+pip install $THIS_DIRECTORY/../dist/*.whl
+
+echo """
+Running tests..."""
+echo "   Clear pipeline variables and ensure that output is not found."
+unset TF_BUILD
+unset GITHUB_ACTIONS
+(
+    if (
+        python3 -c "import logging; logging.warning('Test warning')" 2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=warning]Test warning' ; then
+        echo "Found unexpected Azure pipeline output."
+        exit 1
+    fi
+    if (
+        python3 -c "import logging; logging.error('Test error')"  2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=error]Test error' ; then
+        echo "Found unexpected Azure pipeline output."
+        exit 1
+    fi
+    if (
+        python3 -c "import logging; logging.critical('Test critical')" 2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=error]Test critical' ; then
+        echo "Found unexpected Azure pipeline output."
+        exit 1
+    fi
+)
+
+(
+    if (
+        python3 -c "import logging; logging.warning('Test warning')" 2>&1 1>&2
+    ) | grep -F '::warning::Test warning' ; then
+        echo "Found unexpected GitHub pipeline output."
+        exit 1
+    fi
+    if (
+        python3 -c "import logging; logging.error('Test error')"  2>&1 1>&2
+    ) | grep -F '::error::Test error' ; then
+        echo "Found unexpected GitHub pipeline output."
+        exit 1
+    fi
+    if (
+        python3 -c "import logging; logging.critical('Test critical')" 2>&1 1>&2
+    ) | grep -F '::error::Test critical' ; then
+        echo "Found unexpected GitHub pipeline output."
+        exit 1
+    fi
+)
+
+echo "   Check if Azure pipeline output is found"
+(
+    export TF_BUILD=1
+    (
+        python3 -c "import logging; logging.warning('Test warning')" 2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=warning]Test warning' > nul
+    (
+        python3 -c "import logging; logging.error('Test error')"  2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=error]Test error' > nul
+    (
+        python3 -c "import logging; logging.critical('Test critical')" 2>&1 1>&2
+    ) | grep -F '##vso[task.logissue type=error]Test critical' > nul
+)
+
+echo "   Check if GitHub pipeline output is found"
+(
+    export GITHUB_ACTIONS=1
+    (
+        python3 -c "import logging; logging.warning('Test warning')" 2>&1 1>&2
+    ) | grep -F '::warning::Test warning' > nul
+    (
+        python3 -c "import logging; logging.error('Test error')" 2>&1 1>&2
+    ) | grep -F '::error::Test error' > nul
+    (
+        python3 -c "import logging; logging.critical('Test critical')" 2>&1 1>&2
+    ) | grep -F '::error::Test critical' > nul
+)
